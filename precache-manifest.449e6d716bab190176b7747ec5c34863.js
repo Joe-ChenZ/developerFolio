59c93ecf93afcf92acd7ccc014dad4cb35679290
Chen, Zhao <zchen827@gatech.edu>
2022-12-21 16:44:13 -0500

    Updates

---
+++ precache-manifest.449e6d716bab190176b7747ec5c34863.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553af0c58bacca55f1e146e8b5e9423c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d86b26459ad6114c7924",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e278149619a2af81f158",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "3a20a192e23933e6b68e",
+    "url": "/static/css/main.b8c49e0c.chunk.css"
+  },
+  {
+    "revision": "52adeae056931d282839",
+    "url": "/static/js/2.705ca785.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.705ca785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86b26459ad6114c7924",
+    "url": "/static/js/3.a7c4d7f8.chunk.js"
+  },
+  {
+    "revision": "e278149619a2af81f158",
+    "url": "/static/js/4.4897c883.chunk.js"
+  },
+  {
+    "revision": "3a20a192e23933e6b68e",
+    "url": "/static/js/main.cf266fb3.chunk.js"
+  },
+  {
+    "revision": "d5d35a464ac6b5ca1857",
+    "url": "/static/js/runtime-main.d51ebfdf.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "1c8c76dbe7716a586401448611bdb0a7",
+    "url": "/static/media/Cooking.1c8c76db.jpg"
+  },
+  {
+    "revision": "ada45412e428b12ef6c031e9b15eb0b6",
+    "url": "/static/media/GT_logo.ada45412.png"
+  },
+  {
+    "revision": "e5cb1381b0256eef143f6a83f9c00d8f",
+    "url": "/static/media/GT_word_logo.e5cb1381.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d841a661a371b6ad3c65efedb0c94e83",
+    "url": "/static/media/Publix_Half.d841a661.JPG"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "917635bbf05864b572a4c675535bb945",
+    "url": "/static/media/gt-merit-badge-faculty-honors2.917635bb.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "f88528d3d202c75d08c2bb48e8a97744",
+    "url": "/static/media/moe-logo.f88528d3.jpg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "878b88ba2f847ca733928b4c68c171d0",
+    "url": "/static/media/sketching.878b88ba.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
